b29de3b7ba484d915daee152afc2fdbbebed5328
panayot-cankov <panayot.cankov@progress.com>
2023-10-16 21:40:13 +0300

    feat: log tree with jest lifecycle

--- packages/@progress/roadkill/utils.ts
+++ packages/@progress/roadkill/utils.ts
@@ -14,9 +14,20 @@ export type URL = string;
 export async function delay(milliseconds: number, signal?: AbortSignal, overrideUseImplicitSignal?: boolean): Promise<void> {
     signal = withImplicitSignal(signal, overrideUseImplicitSignal);
     try {
-        await new Promise((resolve, reject) => {
-            signal?.addEventListener("abort", () => reject(signal.reason));
-            setTimeout(resolve, milliseconds);
+        await new Promise<void>((resolve, reject) => {
+            let timeout: NodeJS.Timeout;
+            const onSignal = () => {
+                clearTimeout(timeout);
+                signal?.removeEventListener("abort", onSignal);
+                reject(signal.reason);
+            }
+            const onTimeout = () => {
+                clearTimeout(timeout);
+                signal?.removeEventListener("abort", onSignal);
+                resolve();
+            }
+            timeout = setTimeout(onTimeout, milliseconds);
+            signal?.addEventListener("abort", onSignal);
         });
     } catch(cause) {
         throw new Error("Delay was aborted.", { cause });
@@ -54,23 +65,35 @@ export function withImplicitSignal(signal?: AbortSignal, overrideUseImplicitSign
     return globalSignal ?? signal;
 }
 
-export interface TestState {
+export interface State {
     /**
      * The global implicit signal for this test or hook.
      */
     signal?: AbortSignal;
-
-    testSignal?: AbortSignal;
-    hookSignal?: AbortSignal;
-    testFullName?: string;
-    testShortName?: string;
-    testNameChain?: string[];
-
-    testState?: "pass" | "fail";
+    test?: TestState;
+    hook?: HookState;
 }
 
-export function getState(): TestState {
+export interface TestState {
+    readonly names: ReadonlyArray<string>;
+    readonly fullName: string;
+    readonly testName: string;
+    readonly status: "started" | "fail" | "pass";
+    readonly error?: Error;
+}
+
+export interface HookState {
+    readonly names: ReadonlyArray<string>;
+    readonly fullName: string;
+    readonly hookName: string;
+    readonly status: "started" | "fail" | "pass";
+    readonly error?: Error;
+}
+
+export function getState(): State {
     return {
-        signal: global["@progress/roadkill/utils:signal"]
+        signal: global["@progress/roadkill/utils:signal"],
+        test: global["@progress/roadkill/utils:test"],
+        hook: global["@progress/roadkill/utils:hook"]
     };
 }
